54f2ca64ce0ed21679c558eee431a957decadab9
Abhi86shek <sonkar.abhishek86@gmail.com>
2021-06-08 15:50:43 +0530

    Adding Vector3

---
+++ Runtime/ScriptableVector3.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+
+namespace SAS.ScriptableTypes
+{
+    [CreateAssetMenu(menuName = "SAS/ScriptableTypes/Vector3")]
+    public class ScriptableVector3 : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [SerializeField] private Vector3 m_InitialValue = default;
+        [NonSerialized] public Vector3 runtimeValue;
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            runtimeValue = m_InitialValue;
+        }
+    }
+}
