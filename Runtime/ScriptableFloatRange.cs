31ba0d731af8e887707540877b6b0e850e77f056
Abhi86shek <sonkar.abhishek86@gmail.com>
2021-06-10 11:10:03 +0530

    Adding scriptable intRange and float range

---
+++ Runtime/ScriptableFloatRange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using SAS.Utilities;
+
+namespace SAS.ScriptableTypes
+{
+    [CreateAssetMenu(menuName = "SAS/ScriptableTypes/FloatRange")]
+    public class ScriptableFloatRange : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [SerializeField] private floatRange m_InitialValue = default;
+        [NonSerialized] public floatRange runtimeValue;
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            runtimeValue = m_InitialValue;
+        }
+    }
+}
---
+++ Runtime/ScriptableIntRange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using SAS.Utilities;
+
+namespace SAS.ScriptableTypes
+{
+    [CreateAssetMenu(menuName = "SAS/ScriptableTypes/IntRange")]
+    public class ScriptableIntRange : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [SerializeField] private intRange m_InitialValue = default;
+        [NonSerialized] public intRange runtimeValue;
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            runtimeValue = m_InitialValue;
+        }
+    }
+}
